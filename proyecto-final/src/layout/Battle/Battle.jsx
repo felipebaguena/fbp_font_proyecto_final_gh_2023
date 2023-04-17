cf5fca5e176b08acbff721a99042f17acf1eeec1
felipebaguena <felipebaguena@gmail.com>
2023-04-17 09:31:05 +0200

    feat: select items and add attack and defense modifiers to battle logic

--- proyecto-final/src/layout/Battle/Battle.jsx
+++ proyecto-final/src/layout/Battle/Battle.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { createBattle, getHeroItems } from '../../services/apiCalls';
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { createBattle, getHeroItems } from "../../services/apiCalls";
 
 export const BattlePage = () => {
   const [battle, setBattle] = useState(null);
   const [isPlayerTurn, setIsPlayerTurn] = useState(true);
   const [showModal, setShowModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
   const token = useSelector((state) => state.auth.token);
   const hasFetchedBattle = useRef(false);
-  const [selectedItem, setSelectedItem] = useState('');
+  const [selectedItem, setSelectedItem] = useState("");
   const [heroItems, setHeroItems] = useState([]);
 
   useEffect(() => {
@@ -17,10 +17,10 @@ export const BattlePage = () => {
       if (!hasFetchedBattle.current) {
         hasFetchedBattle.current = true;
         const data = await createBattle(token);
-        if (data && data.status === 'success') {
+        if (data && data.status === "success") {
           setBattle(data.data);
         } else {
-          console.error('Error fetching battle data:', data);
+          console.error("Error fetching battle data:", data);
         }
       }
     };
@@ -31,22 +31,42 @@ export const BattlePage = () => {
     const fetchItems = async () => {
       if (battle && battle.hero) {
         const items = await getHeroItems(token, battle.hero.id);
-        if (items && items.status === 'success') {
+        if (items && items.status === "success") {
           setHeroItems(items.data);
         } else {
-          console.error('Error fetching hero items:', items);
+          console.error("Error fetching hero items:", items);
         }
       }
     };
     fetchItems();
   }, [battle, token]);
 
+  const getSelectedItemAttackModifier = () => {
+    const selectedItemInt = parseInt(selectedItem);
+    const selectedItemObj = heroItems.find((item) => item.id === selectedItemInt);
+    return selectedItemObj ? selectedItemObj.attack_modifier : 0;
+  };
+  
+  const getSelectedItemDefenseModifier = () => {
+    const selectedItemInt = parseInt(selectedItem);
+    const selectedItemObj = heroItems.find((item) => item.id === selectedItemInt);
+    return selectedItemObj ? selectedItemObj.defense_modifier : 0;
+  };
+  
+
   const handleAttack = async () => {
     if (isPlayerTurn) {
 
-        //ATAQUE DEL HÉROE//
+      //ATAQUE DEL HÉROE//
 
-      const damage = battle.hero.attack * 2;
+      const heroDamage = (battle.hero.attack + getSelectedItemAttackModifier()) * 2;
+      const monsterDefense = battle.monster.defense;
+      const damage = Math.max(0, heroDamage - monsterDefense);
+  
+      console.log("Ataque del héroe:", heroDamage);
+      console.log("Defensa del monstruo:", monsterDefense);
+      console.log("Daño realizado al monstruo:", damage);
+  
       const newMonsterHp = Math.max(0, battle.monster.health - damage);
       setBattle((prevState) => ({
         ...prevState,
@@ -55,20 +75,27 @@ export const BattlePage = () => {
           health: newMonsterHp,
         },
       }));
-
+  
       setIsPlayerTurn(false);
-
+  
       if (newMonsterHp <= 0) {
         setShowModal(true);
-        setModalMessage('¡Has vencido al monstruo!');
+        setModalMessage("¡Has vencido al monstruo!");
         return;
       }
-
-        //ATAQUE DEL MONSTRUO//
+  
+      //ATAQUE DEL MONSTRUO//
 
       setTimeout(() => {
-        const monsterDamage = battle.monster.attack;
-        const newHeroHp = Math.max(0, battle.hero.health - monsterDamage);
+        const monsterDamage = battle.monster.attack * 2;
+        const heroDefense = battle.hero.defense + getSelectedItemDefenseModifier();
+        const heroDamageTaken = Math.max(0, monsterDamage - heroDefense);
+  
+        console.log("Ataque del monstruo:", monsterDamage);
+        console.log("Defensa del héroe (incluye modificador de objeto):", heroDefense);
+        console.log("Daño realizado al héroe:", heroDamageTaken);
+  
+        const newHeroHp = Math.max(0, battle.hero.health - heroDamageTaken);
         setBattle((prevState) => ({
           ...prevState,
           hero: {
@@ -76,16 +103,17 @@ export const BattlePage = () => {
             health: newHeroHp,
           },
         }));
-
+  
         if (newHeroHp <= 0) {
           setShowModal(true);
-          setModalMessage('Tu héroe ha sido derrotado.');
+          setModalMessage("Tu héroe ha sido derrotado.");
         } else {
           setIsPlayerTurn(true);
         }
       }, 2000);
     }
-  };
+  };  
+  
 
   if (!battle) {
     return <div>Loading...</div>;
@@ -98,7 +126,10 @@ export const BattlePage = () => {
         {battle.hero.name} vs {battle.monster.name}
       </h2>
       <p>Vida del héroe: {battle.hero.health}</p>
-      <select value={selectedItem} onChange={(e) => setSelectedItem(e.target.value)}>
+      <select
+        value={selectedItem}
+        onChange={(e) => setSelectedItem(e.target.value)}
+      >
         <option value="">Selecciona un objeto</option>
         {heroItems.map((item) => (
           <option key={item.id} value={item.id}>

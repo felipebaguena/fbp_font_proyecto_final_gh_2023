12d5375ef88f83591fd1a685e17da3a4df883600
felipebaguena <felipebaguena@gmail.com>
2023-04-29 12:40:08 +0200

    feat: clean code: BattleControls component in Battle

---
+++ proyecto-final/src/components/BattleComponents/BattleControls.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export function BattleControls({
+  handleAttack,
+  handleDefense,
+  openInventoryModal,
+  selectedItem,
+  findItemById,
+  getBackgroundColorByRarity,
+  heroPotions,
+  handleUsePotion,
+  PotionComponent,
+  battle,
+}) {
+  return (
+    <div className="d-flex justify-content-around mt-3 buttons-battle-container" style={{ marginTop: '1rem' }}>
+      <div className="action-buttons-container">
+        <div>
+          <p className="battle-title">Batalla en {battle.stage.name}</p>
+        </div>
+        <Button variant="dark" onClick={handleAttack} className="attack-button">
+          Atacar
+        </Button>
+        <Button variant="dark" onClick={handleDefense} className="attack-button defense-button">
+          Defensa
+        </Button>
+      </div>
+      <div className="inventory-potion-container">
+        <p className="battle-title">Inventario</p>
+        <Button
+          variant="dark"
+          onClick={openInventoryModal}
+          className={`attack-button inventory-button me-3 ${
+            selectedItem ? getBackgroundColorByRarity(findItemById(selectedItem).rare) : ''
+          }`}
+        >
+          {selectedItem ? findItemById(selectedItem).name : 'Selecciona un objeto'}
+        </Button>
+        <div className="d-flex justify-content-center potion-button">
+          {Array.from({ length: 4 }, (_, index) => (
+            <PotionComponent
+              key={index}
+              index={index}
+              handleUsePotion={handleUsePotion}
+              isActive={index < heroPotions}
+              isClickable={index < heroPotions}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- proyecto-final/src/layout/Battle/Battle.jsx
+++ proyecto-final/src/layout/Battle/Battle.jsx
@@ -15,6 +15,7 @@ import {
 } from "../../services/apiCalls";
 import { BattleModal } from "./BattleModal";
 import { BattleTV } from "../../components/BattleTV/BattleTV";
+import BattleControls from "../../components/BattleComponents/BattleControls";
 
 export const BattlePage = () => {
   const [battle, setBattle] = useState(null);
@@ -488,61 +489,18 @@ export const BattlePage = () => {
         <Col xs={11} sm={10} lg={8} className="controls-container">
           <Col className="mt-3 ">
             {isPlayerTurn && (
-              <div
-                className="d-flex justify-content-around mt-3 buttons-battle-container"
-                style={{ marginTop: "1rem" }}
-              >
-                <div className="action-buttons-container">
-                  <div>
-                    <p className="battle-title">
-                      Batalla en {battle.stage.name}
-                    </p>
-                  </div>
-                  <Button
-                    variant="dark"
-                    onClick={handleAttack}
-                    className="attack-button"
-                  >
-                    Atacar
-                  </Button>
-                  <Button
-                    variant="dark"
-                    onClick={handleDefense}
-                    className="attack-button defense-button"
-                  >
-                    Defensa
-                  </Button>
-                </div>
-                <div className="inventory-potion-container">
-                  <p className="battle-title">Inventario</p>
-                  <Button
-                    variant="dark"
-                    onClick={openInventoryModal}
-                    className={`attack-button inventory-button me-3 ${
-                      selectedItem
-                        ? getBackgroundColorByRarity(
-                            findItemById(selectedItem).rare
-                          )
-                        : ""
-                    }`}
-                  >
-                    {selectedItem
-                      ? findItemById(selectedItem).name
-                      : "Selecciona un objeto"}
-                  </Button>
-                  <div className="d-flex justify-content-center potion-button">
-                    {Array.from({ length: 4 }, (_, index) => (
-                      <Potion
-                        key={index}
-                        index={index}
-                        handleUsePotion={handleUsePotion}
-                        isActive={index < heroPotions}
-                        isClickable={index < heroPotions}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
+              <BattleControls
+                handleAttack={handleAttack}
+                handleDefense={handleDefense}
+                openInventoryModal={openInventoryModal}
+                selectedItem={selectedItem}
+                findItemById={findItemById}
+                getBackgroundColorByRarity={getBackgroundColorByRarity}
+                heroPotions={heroPotions}
+                handleUsePotion={handleUsePotion}
+                PotionComponent={Potion}
+                battle={battle}
+              />
             )}
             <div className="modal-monster-turn">
               {!isPlayerTurn && !showModal && (

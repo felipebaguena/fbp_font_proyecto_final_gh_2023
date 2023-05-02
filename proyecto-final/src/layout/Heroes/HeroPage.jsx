47772c5be8ee41d60e7877842da8ac1a08f77ec5
felipebaguena <felipebaguena@gmail.com>
2023-05-02 13:53:09 +0200

    fix: delete renderInventoryModal in heropage and myheroes

--- proyecto-final/src/layout/Heroes/HeroPage.jsx
+++ proyecto-final/src/layout/Heroes/HeroPage.jsx
@@ -11,7 +11,6 @@ import {
   removeItemFromHero,
 } from "../../services/apiCalls";
 import { Card, Button, Modal } from "react-bootstrap";
-import InventoryModal from "../../components/HeroComponents/InventoryHeroModal";
 import InventoryHeroModal from "../../components/HeroComponents/InventoryHeroModal";
 
 export const HeroPage = () => {
@@ -221,86 +220,6 @@ export const HeroPage = () => {
       </Modal>
     );
   };
-
-  // const renderInventoryModal = () => {
-  //   if (!selectedHero) return null;
-  //   return (
-  //     <Modal
-  //       show={showInventoryModal}
-  //       onHide={handleCloseInventoryModal}
-  //       centered
-  //       className="custom-modal"
-  //     >
-  //       <Modal.Header closeButton>
-  //         <Modal.Title className="modal-title">
-  //           Inventario de {selectedHero.name}
-  //         </Modal.Title>
-  //       </Modal.Header>
-  //       <Modal.Body>
-  //         <div className="list-group">
-  //           {selectedHero.items.map((item, index) => (
-  //             <React.Fragment key={index}>
-  //               <Button
-  //                 variant="dark"
-  //                 className={`custom-button ${
-  //                   selectedItemIndex === index ? "custom-button-select" : ""
-  //                 } ${getBackgroundColorByRarity(item.rare)}`}
-  //                 onClick={() =>
-  //                   selectedItemIndex === index
-  //                     ? setSelectedItemIndex(null)
-  //                     : setSelectedItemIndex(index)
-  //                 }
-  //               >
-  //                 {item.name}
-  //               </Button>
-  //               {selectedItemIndex === index && (
-  //                 <div className="custom-inventory-text">
-  //                   <p>{selectedHero.items[selectedItemIndex].description}</p>
-  //                   <p>
-  //                     Modificador de Ataque:{" "}
-  //                     {selectedHero.items[selectedItemIndex].attack_modifier}
-  //                   </p>
-  //                   <p>
-  //                     Modificador de Defensa:{" "}
-  //                     {selectedHero.items[selectedItemIndex].defense_modifier}
-  //                   </p>
-  //                   <p>
-  //                     Rareza:{" "}
-  //                     {translateRarity(
-  //                       selectedHero.items[selectedItemIndex].rare
-  //                     )}
-  //                   </p>
-  //                   {selectedItemIndex !== null && (
-  //                     <Button
-  //                       variant="danger"
-  //                       className="custom-button custom-button-remove"
-  //                       onClick={() =>
-  //                         handleShowDeleteConfirmModal(
-  //                           selectedHero.items[selectedItemIndex]
-  //                         )
-  //                       }
-  //                     >
-  //                       Eliminar ítem
-  //                     </Button>
-  //                   )}
-  //                 </div>
-  //               )}
-  //             </React.Fragment>
-  //           ))}
-  //         </div>
-  //       </Modal.Body>
-  //       <Modal.Footer>
-  //         <Button
-  //           variant="secondary"
-  //           className="custom-button custom-button-close"
-  //           onClick={handleCloseInventoryModal}
-  //         >
-  //           Cerrar
-  //         </Button>
-  //       </Modal.Footer>
-  //     </Modal>
-  //   );
-  // };
 
   const renderHeroCard = (hero, index) => {
     console.log("Rendering hero", hero.id);
--- proyecto-final/src/layout/Profile/MyHeroes.jsx
+++ proyecto-final/src/layout/Profile/MyHeroes.jsx
@@ -7,8 +7,6 @@ import {
   createHero,
   getHeroesAndItems,
   getAllHeroImages,
-  selectHero,
-  getHeroImage,
   deleteHero,
   removeItemFromHero,
 } from "../../services/apiCalls";
@@ -103,15 +101,6 @@ export const HeroesAndItems = () => {
   useEffect(() => {
     console.log("Current selectedHeroImageId:", selectedHeroImageId);
   }, [selectedHeroImageId]);
-
-  // const handleSelectHero = async (heroId) => {
-  //   const result = await selectHero(token, heroId);
-  //   if (result && result.status === "success") {
-  //     navigate("/battle");
-  //   } else {
-  //     console.error("Error selecting hero:", result);
-  //   }
-  // };
 
   useEffect(() => {
     const fetchHeroImages = async () => {
@@ -307,85 +296,6 @@ export const HeroesAndItems = () => {
     setShowDeleteConfirmModal(true);
   };
 
-  // const renderInventoryModal = () => {
-  //   if (!selectedHero) return null;
-  //   return (
-  //     <Modal
-  //       show={showInventoryModal}
-  //       onHide={handleCloseInventoryModal}
-  //       centered
-  //       className="custom-modal"
-  //     >
-  //       <Modal.Header closeButton>
-  //         <Modal.Title className="modal-title">
-  //           Inventario de {selectedHero.name}
-  //         </Modal.Title>
-  //       </Modal.Header>
-  //       <Modal.Body>
-  //         <div className="list-group">
-  //           {selectedHero.items.map((item, index) => (
-  //             <React.Fragment key={index}>
-  //               <Button
-  //                 variant="dark"
-  //                 className={`custom-button ${
-  //                   selectedItemIndex === index ? "custom-button-select" : ""
-  //                 } ${getBackgroundColorByRarity(item.rare)}`}
-  //                 onClick={() =>
-  //                   selectedItemIndex === index
-  //                     ? setSelectedItemIndex(null)
-  //                     : setSelectedItemIndex(index)
-  //                 }
-  //               >
-  //                 {item.name}
-  //               </Button>
-  //               {selectedItemIndex === index && (
-  //                 <div className="custom-inventory-text">
-  //                   <p>{selectedHero.items[selectedItemIndex].description}</p>
-  //                   <p>
-  //                     Modificador de Ataque:{" "}
-  //                     {selectedHero.items[selectedItemIndex].attack_modifier}
-  //                   </p>
-  //                   <p>
-  //                     Modificador de Defensa:{" "}
-  //                     {selectedHero.items[selectedItemIndex].defense_modifier}
-  //                   </p>
-  //                   <p>
-  //                     Rareza:{" "}
-  //                     {translateRarity(
-  //                       selectedHero.items[selectedItemIndex].rare
-  //                     )}
-  //                   </p>
-  //                   {selectedItemIndex !== null && (
-  //                     <Button
-  //                       variant="danger"
-  //                       className="custom-button custom-button-remove"
-  //                       onClick={() =>
-  //                         handleShowDeleteConfirmModal(
-  //                           selectedHero.items[selectedItemIndex]
-  //                         )
-  //                       }
-  //                     >
-  //                       Eliminar ítem
-  //                     </Button>
-  //                   )}
-  //                 </div>
-  //               )}
-  //             </React.Fragment>
-  //           ))}
-  //         </div>
-  //       </Modal.Body>
-  //       <Modal.Footer>
-  //         <Button
-  //           variant="secondary"
-  //           className="custom-button custom-button-close"
-  //           onClick={handleCloseInventoryModal}
-  //         >
-  //           Cerrar
-  //         </Button>
-  //       </Modal.Footer>
-  //     </Modal>
-  //   );
-  // };
 
   const renderNewHeroCard = () => {
     return (
